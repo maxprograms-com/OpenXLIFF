f3f1c1e5e7758b7a0e9287ac707c6e6f7aa6fcd5
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-09-26 08:35:30 -0300

    Updated BCP47J

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -21,8 +21,8 @@ public class Constants {
 
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
-	public static final String VERSION = "6.4.0";
-	public static final String BUILD = "20260921_1837";
+	public static final String VERSION = "6.4.1";
+	public static final String BUILD = "20260926_0834";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
